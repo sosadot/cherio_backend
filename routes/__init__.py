6ba642b7f8bc3d6baee315f58bc34a5fe120f0be
enigma550 <62433601+enigma550@users.noreply.github.com>
2025-04-15 01:18:19 +0200

    add imports to __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,5 +1,9 @@
 # routes/__init__.py
 from . import auth
-from . import user
-from . import news
 from . import general
+from . import image_proxy
+from . import leaderboard
+from . import news
+from . import shop
+from . import user
+
